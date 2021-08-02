b699c1c36ad7a21ba13f3e80f3ebade0b572851a
Webdornald <webdornald@gmail.com>
2021-08-02 17:48:54 +0900

    singup/login done

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+
 from . import models
 
 
@@ -19,33 +22,43 @@ class LoginForm(forms.Form):
             self.add_error('email', forms.ValidationError('User does not exist'))
 
 
-class SignUpForm(forms.ModelForm):
+# UserCreationForm을 사용하는 경우
+class SignUpForm(UserCreationForm):
+    username = forms.EmailField(label='Email')
+
     class Meta:
         model = models.User
-        fields = ['first_name', 'last_name', 'email']
-
-    password1 = forms.CharField(widget=forms.PasswordInput, label='Password')
-    password2 = forms.CharField(widget=forms.PasswordInput, label='Confirm Password')
-
-    def clean_password2(self):
-        password1 = self.cleaned_data.get('password1')
-        password2 = self.cleaned_data.get('password2')
-
-        if password1 != password2:
-            raise forms.ValidationError('Password confirmation does not match')
-        else:
-            return password1
-
-    def save(self, *args, **kwargs):
-        user = super().save(commit=False)
-        email = self.cleaned_data.get('email')
-        password = self.cleaned_data.get('password1')
-        user.username = email
-        user.set_password(password)
-        user.save()
+        fields = ['username', 'password1', 'password2']
 
 
+# ModelForm을 상속받는 경우
+# class SignUpForm(forms.ModelForm):
+#     class Meta:
+#         model = models.User
+#         fields = ['first_name', 'last_name', 'email']
+#
+#     password1 = forms.CharField(widget=forms.PasswordInput, label='Password')
+#     password2 = forms.CharField(widget=forms.PasswordInput, label='Confirm Password')
+#
+#     def clean_password2(self):
+#         password1 = self.cleaned_data.get('password1')
+#         password2 = self.cleaned_data.get('password2')
+#
+#         if password1 != password2:
+#             raise forms.ValidationError('Password confirmation does not match')
+#         else:
+#             return password1
+#
+#     def save(self, *args, **kwargs):
+#         user = super().save(commit=False)
+#         email = self.cleaned_data.get('email')
+#         password = self.cleaned_data.get('password1')
+#         user.username = email
+#         user.set_password(password)
+#         user.save()
 
+
+# 그냥 폼을 쓰는 경우
 # class SignUpForm(forms.Form):
 #     first_name = forms.CharField(max_length=80)
 #     last_name = forms.CharField(max_length=80)
--- users/views.py
+++ users/views.py
@@ -5,6 +5,7 @@ from django.urls import reverse_lazy
 from django.views import View
 from django.views.generic import FormView
 from django.contrib.auth import authenticate, login, logout
+from django.core.files.base import ContentFile
 from . import forms, models
 
 
@@ -63,7 +64,7 @@ class SignUpView(FormView):
 
     def form_valid(self, form):
         form.save()
-        email = form.cleaned_data.get('email')
+        email = form.cleaned_data.get('username')  # ModelForm 사용시 'username'을 'email'로 바꿔야 한다.
         password = form.cleaned_data.get('password1')
         user = authenticate(self.request, username=email, password=password)
         if user is not None:
@@ -129,7 +130,14 @@ def github_callback(request):
                         if user.login_method != models.User.LOGIN_GITHUB:
                             raise GithubException()
                     except models.User.DoesNotExist:
-                        user = models.User.objects.create(email=email, first_name=name, username=email, bio=bio, login_method=models.User.LOGIN_GITHUB)
+                        user = models.User.objects.create(
+                            email=email,
+                            first_name=name,
+                            username=email,
+                            bio=bio,
+                            login_method=models.User.LOGIN_GITHUB,
+                            email_verified=True,
+                        )
                         user.set_unusable_password()
                         user.save()
                     login(request, user)
@@ -140,4 +148,56 @@ def github_callback(request):
             raise GithubException()
     except GithubException:
         # send error message
+        return redirect('users:login')
+
+
+def kakao_login(request):
+    REST_API_KEY = os.environ.get('KAKAO_ID')
+    REDIRECT_URI = 'http://127.0.0.1:8000/users/login/kakao/callback'
+    return redirect(f'https://kauth.kakao.com/oauth/authorize?client_id={REST_API_KEY}&redirect_uri={REDIRECT_URI}&response_type=code')
+
+
+class KakaoException(Exception):
+    pass
+
+
+def kakao_callback(request):
+    try:
+        code = request.GET.get('code')
+        client_id = os.environ.get('KAKAO_ID')
+        redirect_uri = 'http://127.0.0.1:8000/users/login/kakao/callback'
+        token_request = requests.get(f'https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id={client_id}&redirect_uri={redirect_uri}&code={code}')
+        token_json = token_request.json()
+        error = token_json.get('error', None)
+        if error is not None:
+            raise KakaoException()
+        access_token = token_json.get('access_token')
+        profile_request = requests.get('https://kapi.kakao.com/v2/user/me', headers={'Authorization': f'Bearer {access_token}'})
+        profile_json = profile_request.json()
+        email = profile_json.get('kakao_account').get('email')
+        if email is None:
+            raise KakaoException()
+        properties = profile_json.get('properties')
+        nickname = properties.get('nickname')
+        profile_image = properties.get('profile_image')
+        try:
+            user = models.User.objects.get(email=email)
+            if user.login_method != models.User.LOGIN_KAKAO:
+                raise KakaoException()
+        except models.User.DoesNotExist:
+            user = models.User.objects.create(
+                email=email,
+                username=email,
+                first_name=nickname,
+                login_method=models.User.LOGIN_KAKAO,
+                email_verified=True,
+            )
+            user.set_unusable_password()
+            user.save()
+            if profile_image is not None:
+                photo_request = requests.get(profile_image)
+                user.avatar.save(f'{nickname}-avatar.jpeg', ContentFile(photo_request.content))
+        login(request, user)
+        return redirect('core:home')
+    except KakaoException:
         return redirect('users:login')
